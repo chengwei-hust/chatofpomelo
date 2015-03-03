05677c44cac8cc60f754981b1a05c3e4e41f31fd
chengwei <chengwei.hust@gmail.com>
2015-03-03 18:27:03 +0800

    Update

--- game-server/app/util/dispatcher.js
+++ game-server/app/util/dispatcher.js
@@ -1,18 +1,7 @@
 var crc = require('crc');
 
-//module.exports.dispatch = function(uid, connectors) {
-//	var index = Math.abs(crc.crc32(uid)) % connectors.length;
-//	return connectors[index];
-//};
-
-
 module.exports.dispatch = function(uid, connectors) {
-    console.info(uid);
-    console.info(connectors.length);
-
- //   var index = Math.abs(crc.crc32(uid)) % connectors.length;
-    var index = parseInt(uid) % connectors.length;
-    console.info(index);
+    var index = Math.abs(crc.crc32(uid)) % connectors.length;
+ //   var index = parseInt(uid) % connectors.length;   数字型uid
     return connectors[index];
 };
-
