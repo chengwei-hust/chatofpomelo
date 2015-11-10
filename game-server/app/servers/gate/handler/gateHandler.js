a5d009f0f50d5119976949e3cf0d504a0a86b511
chengwei <chengwei.hust@gmail.com>
2015-11-10 12:35:05 +0800

    accesstoken

--- game-server/app/servers/gate/handler/gateHandler.js
+++ game-server/app/servers/gate/handler/gateHandler.js
@@ -1,4 +1,5 @@
 var dispatcher = require('../../../util/dispatcher');
+var ticketService = require('../../../service/ticketService')
 
 module.exports = function(app) {
 	return new Handler(app);
@@ -20,6 +21,14 @@ var handler = Handler.prototype;
  */
 handler.queryEntry = function(msg, session, next) {
 
+    var guest = msg.guest;
+    if(!guest) {
+        next(null, {
+            code: 10000,
+            msg:"请传入是否游客身份参数guest"
+        });
+        return;
+    }
     var access_token = msg.access_token;
     if (!access_token) {
         next(null, {
@@ -28,12 +37,12 @@ handler.queryEntry = function(msg, session, next) {
         });
         return;
     }
-
-	var uid = msg.uid;
+	var uid = ticketService.getUserIdByTicket(access_token, guest);
 	if(!uid) {
-		next(null, {
-			code: 500
-		});
+        next(null, {
+            code: 20000,
+            msg:"access_token无效或者已过期，请出重新获取"
+        });
 		return;
 	}
 	// get all connectors
