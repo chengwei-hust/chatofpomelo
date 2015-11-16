cb7ae860a531fe5cf735a442f414c22ae439648c
chengwei <chengwei.hust@gmail.com>
2015-11-16 20:50:54 +0800

    syntax fix

--- game-server/app/servers/gate/handler/gateHandler.js
+++ game-server/app/servers/gate/handler/gateHandler.js
@@ -30,7 +30,7 @@ handler.queryEntry = function(msg, session, next) {
         return;
     }
     var access_token = msg.access_token;
-    if (!access_token) {
+    if (!!access_token) {
         next(null, {
             code: 20000,
             msg:"access_token无效或者已过期，请出重新获取"
@@ -38,7 +38,7 @@ handler.queryEntry = function(msg, session, next) {
         return;
     }
 	var uid = ticketService.getUserIdByTicket(access_token, guest);
-	if(!uid) {
+	if(!!uid) {
         next(null, {
             code: 20000,
             msg:"access_token无效或者已过期，请出重新获取"
